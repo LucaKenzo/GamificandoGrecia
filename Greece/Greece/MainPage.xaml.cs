9e4ccb88e1688e54b13e93859c9a48490a1bc453
Luca Kenzo <luca.kenzo0204@gmail.com>
2024-10-03 12:39:57 -0300

    image button

---
+++ Greece/Greece/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace Greece
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int clickTotal;
+
+        
+
+        void OnImageButtonClicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
